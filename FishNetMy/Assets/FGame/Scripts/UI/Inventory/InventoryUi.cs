44ec3dce5c02f419af944c69baeffb791a071de4
nuofeng <1059362872@qq.com>
2026-04-01 19:15:40 +0800

    完善库存显示和添加逻辑

--- FishNetMy/Assets/FGame/Scripts/UI/Inventory/InventoryUi.cs
+++ FishNetMy/Assets/FGame/Scripts/UI/Inventory/InventoryUi.cs
@@ -5,6 +5,7 @@ using Sirenix.OdinInspector;
 using UnityEngine.UI;
 using TMPro;
 using System;
+using System.Linq;
 
 namespace FGame
 {
@@ -14,7 +15,13 @@ namespace FGame
         private  TextMeshProUGUI InventoryName;
 
         [SerializeField]
+        private Vector2 SlotSize;
+
+        [SerializeField]
         private GridLayoutGroup ItemRoot;
+
+        [SerializeField]
+        private Transform MultiItemRoot;
 
         private string Name;
 
@@ -22,7 +29,7 @@ namespace FGame
         private InventoryBase _inventoryBase;
 
         private List<SlotUi> slotUis = new List<SlotUi>();
-
+        private List<SlotUi> MultislotUis = new List<SlotUi>();
 
         // Start is called before the first frame update
         void Start()
@@ -37,13 +44,18 @@ namespace FGame
         {
 
         }
+
+
         public void Show(InventoryBase inventoryBase)
         {
-            Debug.Log("打开库存");
+            //Debug.Log("打开库存");
             this.gameObject.SetActive(true);
             _inventoryBase = inventoryBase;
+            InventoryName.text = inventoryBase.InventoryName;
             var size = inventoryBase.InventorySize;
-            Debug.Log(size);
+            var Items = inventoryBase.ClientItems;
+
+            //Debug.Log(size);
             ItemRoot.constraintCount = size.x;
             int allSlotCount = size.x * size.y;
             for (int i=0;i< allSlotCount;i++)
@@ -51,12 +63,80 @@ namespace FGame
                 var slotUi =  FGFramework.Ins.GetCtr<PoolController>().GetPool<SlotUi>(PoolName.SlotUi.ToString()).Get();
                 slotUis.Add(slotUi);
                 slotUi.transform.parent = ItemRoot.transform;
+                slotUi.transform.localScale = Vector3.one;
+                slotUi.Init(new Vector2Int(i % size.x, (i / size.x)));
             }
 
 
+            for (int i=0;i< slotUis.Count;i++)
+            {
+                //Debug.Log($"{slotUis[i].Index}:{slotUis[i].Item.ItemId}");
+                var curItem = Items[i];
+                //Debug.Log(curItem.Position);
+                if (curItem.ItemId == 0) continue;
+                if (slotUis[i].Item.ItemId ==0 )
+                {                                  
+                    var itemData = FGFramework.Ins.GetCtr<ConfigController>().GetItemData(curItem.ItemId);
+                    int itemSize = itemData.SlotSizeX * itemData.SlotSizeY;
+                    if (itemSize == 1)
+                    {
+                        slotUis[i].UpateUi(curItem);
+                    } 
+                    else if(itemSize>1)
+                    {
+                        var roundSlots = GetRoundSlots(slotUis, curItem,new Vector2(itemData.SlotSizeX, itemData.SlotSizeY));
+                      
+                        for (int n = 0; n < roundSlots.Count; n++)
+                        {
+                            //Debug.Log(roundSlots[n].Index);
+                            roundSlots[n].UpateUi(curItem,false);
+                        }
+                        //Debug.Log("Get");
+                        var MultiSlotUi = FGFramework.Ins.GetCtr<PoolController>().GetPool<SlotUi>(PoolName.SlotUi.ToString()).Get();
+                        MultislotUis.Add(MultiSlotUi);
+                        MultiSlotUi.transform.parent = MultiItemRoot;
+                        MultiSlotUi.transform.localScale = Vector3.one;
+                        MultiSlotUi.Init(new Vector2Int(i % size.x, (i / size.x)), new Vector2(itemData.SlotSizeX, itemData.SlotSizeY), SlotSize, curItem.Position);
+                        MultiSlotUi.UpateUi(curItem);
 
-
+                    }
+                }
+            }
         }
+
+
+        public List<SlotUi> GetRoundSlots(List<SlotUi> AllItems, Item curItem, Vector2 ItemSize)
+        {
+            List<SlotUi> items = new List<SlotUi>();
+            for (int i = 0; i < ItemSize.x; i++)
+            {
+                for (int n = 0; n < ItemSize.y; n++)
+                {
+                    //Debug.Log($"curItem.Position:{curItem.Position}");
+                    Vector2 target = new Vector2Int(curItem.Position.x + i, curItem.Position.y + n);              
+                    var roundSlot = AllItems.FirstOrDefault(item => item.Index == target);
+                    items.Add(roundSlot);
+                }
+            }
+
+            return items;
+        }
+
+
+
+        public void ItemNormalSort()
+        { 
+        
+        
+        
+        }
+
+
+
+
+
+
+
         public void Show(Item[] items,string InventoryName)
         {
             this.gameObject.SetActive(true);
